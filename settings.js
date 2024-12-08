17037583fea6991c891f8ba3f3b5c4f18cc9a65c
Kavishkabox <kdpantabot@outlook.com>
2024-12-08 20:00:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=PvwSAAbC#vQcGWwl1gL-d4WcUfRRZ9xeymXjO_3kUrZF4PoRojdY' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'DNwAwDwC#f_Txn_E4qqbrnIWd9Dz_3q7z-Yg-mPmDVIhNNVEip3U' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
